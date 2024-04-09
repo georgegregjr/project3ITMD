c669dd2733e7403fd561be9c0773ac5d6ed3e738
georgegregjr <gwest1@hawk.iit.edu>
2024-04-09 13:27:34 -0500

    Testing to see if JS works on maps page

--- js/mapsJs.js
+++ js/mapsJs.js
@@ -1,6 +1,13 @@
- var map;
+/* var map;
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.948335, lng: -87.655279},
         zoom: 8
         }};
+        
+        
+*/
+function(){
+  alert(' JS Testing');
+}
+google.maps.event.addDomListener(window, 'load', init);
