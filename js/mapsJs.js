87673c002ea2642c93ef39d0bc8163c5ecd9aef6
georgegregjr <gwest1@hawk.iit.edu>
2024-04-09 13:24:36 -0500

    Basic Map changes still not working probably

---
+++ js/mapsJs.js
@@ -0,0 +1,6 @@
+ var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.948335, lng: -87.655279},
+        zoom: 8
+        }};
