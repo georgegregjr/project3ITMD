1dea5441c20eea281c733922bfdfc319d9f8dae1
georgegregjr <gwest1@hawk.iit.edu>
2024-04-09 13:42:29 -0500

    Updated JS and HTML to fix map

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-/* var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 41.948335, lng: -87.655279},
-        zoom: 8
-        }};
-        
-        
-*/
 function init (){
   alert(' JS Testing');
+  var newMap = document.getElementById('beyond');
+  var mapOptions= {
+    center: {lat: 41.948335, lng: -87.655279},
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      posistion: google.maps.ControlPosistion.BOTTOM_CENTER
+    }
+  };
+  
+  var theMap = new google.maps.Map(newMap, mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', init);
