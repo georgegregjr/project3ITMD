aed409099e1d5ea8d5cc4a9feb00a66cc7790a34
georgegregjr <gwest1@hawk.iit.edu>
2024-04-09 20:35:10 -0500

    Following exactly what google maps api website says

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function initMap (){
+/*function initMap (){
   alert(' JS Testing');
   var newMap = document.getElementById('beyond');
   var mapOptions= {
@@ -13,3 +13,16 @@ function initMap (){
   var theMap = new google.maps.Map(newMap, mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initMap);
+*/
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("beyond"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
